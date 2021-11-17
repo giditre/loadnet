22d6ed3f647801075ce5c0fe75fc3ba43df74dd7
giditre <giditre@gmail.com>
2021-11-17 11:11:33 +0100

    improved replace.py

--- tools/replace.py
+++ tools/replace.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import re
 from pathlib import Path
 import json
@@ -6,13 +8,17 @@ import sys
 ### Command line argument parser
 import argparse
 
-default_group = 5
+default_group = 4
 
-parser = argparse.ArgumentParser()
-parser.add_argument("input_file_name", help=f"input file name")
-parser.add_argument("-G", "--group", type=int, help=f"group number, default: {default_group}", nargs="?", default=default_group)
-parser.add_argument("-y", "--assume-yes", help="automatic yes to prompts", action="store_true", default=False)
-parser.add_argument("-v", "--verbose", help="print result to stdout", action="store_true", default=False)
+parser = argparse.ArgumentParser(description="Replace operations like --G-- or --X+1-- with their results in text files.")
+parser.add_argument("input_file_name_list", type=str, nargs="+", metavar="file_name",
+  help=f"input file name(s)")
+parser.add_argument("-G", "--group", type=int, default=default_group, metavar="G",
+  help=f"group number, default: {default_group}")
+parser.add_argument("-y", "--assume-yes", action="store_true", default=False,
+  help="automatically answer yes to prompts")
+parser.add_argument("-v", "--verbose", action="store_true", default=False,
+  help="print result of replacement to stdout")
 
 if __name__ == "__main__":
 
@@ -27,24 +33,27 @@ if __name__ == "__main__":
   G = args.group
   X = 10*G
 
-  input_file_path = Path(__file__).absolute().parent.parent.joinpath(args.input_file_name)
-  # print(input_file_path)
+  input_file_path_list = [ Path(__file__).absolute().parent.parent.joinpath(in_f) for in_f in args.input_file_name_list ]
+  # print(input_file_path_list)
 
-  output_file_path = input_file_path.parent.joinpath(f"G{G}_{input_file_path.name}")
-  # print(output_file_path)
+  for input_file_path in input_file_path_list:
 
-  file_content = open(input_file_path).read()
+    # print(input_file_path)
 
-  # print(file_content)
+    output_file_path = input_file_path.parent.joinpath(f"G{G}_{input_file_path.name}")
+    # print(output_file_path)
 
-  reg_exp = r"\-\-([GX]|[+]|[0-9])+\-\-"
+    file_content = open(input_file_path).read()
+    # print(file_content)
 
-  while ( m := re.search(reg_exp, file_content) ) is not None:
-    matched_string = m.group()
-    result = eval(matched_string.replace("--", ""))
-    file_content = file_content.replace(matched_string, str(result))
+    reg_exp = r"\-\-([GX]|[+]|[0-9])+\-\-"
 
-  if args.verbose:
-    print(file_content)
+    while ( m := re.search(reg_exp, file_content) ) is not None:
+      matched_string = m.group()
+      result = eval(matched_string.replace("--", ""))
+      file_content = file_content.replace(matched_string, str(result))
 
-  open(output_file_path, "w").write(file_content)
+    if args.verbose:
+      print(file_content)
+
+    open(output_file_path, "w").write(file_content)
